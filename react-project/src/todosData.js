6f4cba326a4d5df526aacbce5daeea22bb9de57f
anglerfishlyy <kavyasamudralaA7@outlook.com>
2024-05-31 22:23:37 +0530

    Setup for GitHub Pages deployment

---
+++ react-project/src/todosData.js
@@ -0,0 +1,68 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "do yoga",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "family time",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Go out and meet new people",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Get in the habit",
+        completed: false
+    },
+    {
+        id: 6,
+        text: "Meditate!!",
+        completed: true
+    },
+    {
+        id: 7,
+        text: "Buy groceries",
+        completed: false
+    },
+    {
+        id: 8,
+        text: "Pay the electricity bill",
+        completed: true
+    },
+    {
+        id: 9,
+        text: "schedule an appointment",
+        completed: false
+    },
+    {
+        id: 10,
+        text: "Finish reading a book",
+        completed: false
+    },
+    {
+        id: 11,
+        text: "Water the plants",
+        completed: false
+    },
+    {
+        id: 12,
+        text: "Plan a vacation",
+        completed: false
+    },
+    {
+        id: 13,
+        text: "Update resume or LinkedIn profile",
+        completed: false
+    }
+];
+export default todosData;
